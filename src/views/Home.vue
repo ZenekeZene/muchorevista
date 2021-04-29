8fae15eaaf6682d1fd68429b1881bdcdc540e8de
ZenekeZene <hectorvillarm@gmail.com>
2021-04-29 18:37:25 +0200

    Axios added. Form via axios.post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,66 @@
     <img class="logo" alt="Mucho logo" src="../assets/logo.svg">
     <p style="text-align: left;">Para Mayo lo sacamos. Prometido.🤞
     <br/>Si quieres que te avisemos, <strong>déjanos un email:</strong></p>
-    <form name="contact" method="POST" data-netlify="true"
-      data-netlify-honeypot="bot-field">
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      @submit.prevent="handleSubmit"
+    >
       <input type="hidden" name="form-name" value="contact">
-      <input type="email" name="email" placeholder="micorreo@ejemplo.com" required/>
-      <textarea name="message" placeholder="Mi mensaje (opcional)"></textarea>
+      <input
+        type="email"
+        name="email"
+        v-model="form.email"
+        placeholder="micorreo@ejemplo.com" required
+      />
+      <textarea
+        name="message"
+        placeholder="Mi mensaje (opcional)"
+        v-model="form.message"
+      ></textarea>
       <button type="submit">Avísame</button>
     </form>
     <a href="mailto:info@revistamucho.com">info@revistamucho.com</a>
   </article>
 </template>
+<script>
+import axios from 'axios'
 
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        message: ''
+      }
+    }
+  },
+  methods: {
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    handleSubmit () {
+      const axiosConfig = {
+        header: { "Content-Type": "application/x-www-form-urlencoded" }
+      };
+      axios.post(
+        "/",
+        this.encode({
+          "form-name": "contact",
+          ...this.form
+        }),
+        axiosConfig
+      );
+    }
+  }
+}
+</script>
 <style lang="scss">
 .logo {
   max-height: 42vh;
