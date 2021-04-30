7d6d0454e64289bb1c879d1dce83311b2905f0fe
ZenekeZene <hectorvillarm@gmail.com>
2021-04-30 14:36:45 +0200

    SubmissionFail.vue

--- src/components/SubmissionFail.vue
+++ src/components/SubmissionFail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Iepaaa, ¡algo ha salido mal! Inténtalo más tarde, porfa.</h1>
+    <h1>Iepaaa, ¡algo ha salido mal!!! Inténtalo más tarde, porfa.</h1>
     <router-link to="/">
       <button>Volver al inicio</button>
     </router-link>
